a15c5c36ffa10875ec5d42239a2c0f1f2ea078af
Manish Dwibedy <manish.dwibedy@gmail.com>
2026-01-21 18:35:32 +0530

    sitemap release

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
   site: 'https://blog.manishd.in',
   base: '/',
   outDir: './dist',
+  integrations: [
+    tailwind(),
+    sitemap()
+  ]
 });
 
